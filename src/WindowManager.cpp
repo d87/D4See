0804aed97f08421e3c0bd28c2d212e17aa806eeb
d87 <d87d87@gmail.com>
2020-06-18 14:18:35 +0700

    Open File Dialog

--- src/WindowManager.cpp
+++ src/WindowManager.cpp
@@ -491,8 +491,54 @@ void WindowManager::ShowPopupMenu(POINT& p) {
     DestroyMenu(popupMenu);
 }
 
+void OpenFileDialog() {
+    HRESULT hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED |
+        COINIT_DISABLE_OLE1DDE);
+    if (SUCCEEDED(hr))
+    {
+        IFileOpenDialog* pFileOpen;
+
+        // Create the FileOpenDialog object.
+        hr = CoCreateInstance(CLSID_FileOpenDialog, NULL, CLSCTX_ALL,
+            IID_IFileOpenDialog, reinterpret_cast<void**>(&pFileOpen));
+
+        if (SUCCEEDED(hr))
+        {
+            // Show the Open dialog box.
+            hr = pFileOpen->Show(NULL);
+
+            // Get the file name from the dialog box.
+            if (SUCCEEDED(hr))
+            {
+                IShellItem* pItem;
+                hr = pFileOpen->GetResult(&pItem);
+                if (SUCCEEDED(hr))
+                {
+                    PWSTR pszFilePath;
+                    hr = pItem->GetDisplayName(SIGDN_FILESYSPATH, &pszFilePath);
+
+                    // Display the file name to the user.
+                    if (SUCCEEDED(hr))
+                    {
+                        MessageBoxW(NULL, pszFilePath, L"File Path", MB_OK);
+                        CoTaskMemFree(pszFilePath);
+                    }
+                    pItem->Release();
+                }
+            }
+            pFileOpen->Release();
+        }
+        CoUninitialize();
+    }
+    return;
+}
+
 void WindowManager::HandleMenuCommand(unsigned int uIDItem) {
     switch (uIDItem) {
+        case ID_OPENFILE: {
+            OpenFileDialog();
+            break;
+        }
         case ID_ZOOMLOCK: {
             zoomLock = !zoomLock;
             break;
