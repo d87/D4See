b568d757d521e245f8bff03c3b840bddeb29fd5b
d87 <d87d87@gmail.com>
2020-11-10 03:37:18 +0700

    Copy config files from etc folder in post-build event

---
+++ etc/bindings.lua
@@ -0,0 +1,36 @@
+BindAction("CTRL-F", "TOGGLEFULLSCREEN");
+BindAction("RETURN", "TOGGLEFULLSCREEN");
+BindAction("MBUTTON3", "TOGGLEFULLSCREEN");
+BindAction("TAB", "TOGGLEBORDERLESS");
+BindAction("CTRL-A", "ALWAYSONTOP");
+
+BindAction("Z", "MOUSEZOOM");
+BindAction("MBUTTON1", "MOUSEPAN");
+BindAction("MBUTTON2", "SHOWMENU");
+BindAction("CTRL-X", "CUTFILE");
+BindAction("CTRL-C", "COPYFILE");
+BindAction("DEL", "DELETEFILE");
+BindAction("SHIFT-DEL", "NUKEFILE");
+BindAction("ESC", "QUIT");
+
+BindAction("SPACE", "NEXTIMAGE");
+BindAction("PAGEDOWN", "NEXTIMAGE");
+BindAction("MWHEELDOWN", "NEXTIMAGE");
+BindAction("PAGEUP", "PREVIMAGE");
+BindAction("MWHEELUP", "PREVIMAGE");
+BindAction("HOME", "FIRSTIMAGE");
+BindAction("END", "LASTIMAGE");
+BindAction("CTRL-L", "ZOOMLOCK");
+
+BindAction("CTRL-SUBTRACT", "TOGGLESCREENFIT");
+
+
+BindAction("CTRL-MWHEELUP", "ZOOMINPOINT");
+BindAction("CTRL-MWHEELDOWN", "ZOOMOUTPOINT");
+
+BindAction("F5", "REFRESHPLAYLIST");
+
+BindAction("UP", "PANUP");
+BindAction("DOWN", "PANDOWN");
+BindAction("LEFT", "PANLEFT");
+BindAction("RIGHT", "PANRIGHT");
---
+++ etc/config.lua
@@ -0,0 +1,8 @@
+CONFIG = {
+    Borderless = false,
+    SortMethod = "ByDateModified",
+    StretchToScreenWidth = true,
+    StretchToScreenHeight = true,
+    ShrinkToScreenWidth = true,
+    ShrinkToScreenHeight = true,
+}
